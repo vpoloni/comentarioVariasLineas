25aa5878a33a6d70a0efd83ae740e03df92e2578
Veronika Polonchak <polonchakveronika@gmail.com>
2022-04-10 14:22:01 +0200

    local commit

---
+++ src/pr3/ComentarioVariasLineas.java
@@ -0,0 +1,17 @@
+package pr3;
+
+public class ComentarioVariasLineas {
+
+	public static void main(String[] args) {
+		
+		//Hacemos iteraciones utilizando el bucle "for"
+		for (int i = 0; i <6; i++)
+			
+			//Mostramos por consola el resultado de cada iteración
+			System.out.println(i + "");
+		System.out.println("FIN DE PROGRAMA");
+
+	}
+
+}
+
